9bafaf9db1bf621445be24b0f23c14fb4795f02f
xuren <1562694805@qq.com>
2023-06-13 20:51:36 +0800

    test1分支

--- src/main/java/com/personal/test1.java
+++ src/main/java/com/personal/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("Hi, I'm XuRen!");
+        System.out.println("This is test1...");
     }
 }
