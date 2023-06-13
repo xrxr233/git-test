6ecad3f61c7399c05c1a9a976ad125abc60e0e6a
xuren <1562694805@qq.com>
2023-06-13 20:47:14 +0800

    第一次修改

--- src/main/java/com/personal/test1.java
+++ src/main/java/com/personal/test1.java
@@ -3,5 +3,6 @@ package com.personal;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("Hi, I'm XuRen!");
     }
 }
